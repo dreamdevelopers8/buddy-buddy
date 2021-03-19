ce3ae6ab4c2b3850c421486d97ca4a777e72761f
unknown <vigneshprsd@gmail.com>
2021-03-19 23:16:29 +0530

    vignesh -- package.json changed

--- routes/index.router.js
+++ routes/index.router.js
@@ -7,4 +7,5 @@ const ctrlUser = require("../controllers/user.controller");
 router.post("/store-user", ctrlUser.store_user_answer);
 router.post("/store-friends-details/:user_id", ctrlUser.store_friends_details);
 
+
 module.exports = router;
